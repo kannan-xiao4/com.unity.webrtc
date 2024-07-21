dfdeec81e6b50d880d14f62539b2782ad66ecc42
Takashi KANNAN <26959415+kannan-xiao4@users.noreply.github.com>
2024-07-21 17:10:29 +0900

    fix project

--- Plugin~/WebRTCPlugin/GraphicsDevice/Vulkan/VulkanUtility.cpp
+++ Plugin~/WebRTCPlugin/GraphicsDevice/Vulkan/VulkanUtility.cpp
@@ -103,14 +103,12 @@ namespace webrtc
         VkMemoryDedicatedAllocateInfo dedicatedAllocateInfo = {};
         if (exportHandle)
         {
-#if UNITY_ANDROID
             // When AllocateMemory is executed, Android requires the VkImage to be used as additional information.
             dedicatedAllocateInfo.sType  = VK_STRUCTURE_TYPE_MEMORY_DEDICATED_ALLOCATE_INFO;
             dedicatedAllocateInfo.pNext  = nullptr;
             dedicatedAllocateInfo.buffer = VK_NULL_HANDLE;
             dedicatedAllocateInfo.image  = unityVulkanImage->image;
             exportInfo.pNext = &dedicatedAllocateInfo;
-#endif
             exportInfo.sType = VK_STRUCTURE_TYPE_EXPORT_MEMORY_ALLOCATE_INFO;
             exportInfo.handleTypes = EXTERNAL_MEMORY_HANDLE_SUPPORTED_TYPE;
             allocInfo.pNext = &exportInfo;
